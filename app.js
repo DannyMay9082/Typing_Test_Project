ae3df849b5a73dc4ece12a0dba9d70067eb47d9d
DannyMay9082 <Danny.May.9012@gmail.com>
2022-06-24 20:39:37 +0400

    delete two rows after finishing them

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 import { words as list } from './words.js'
 
-//başlanğıc üçün müəyyən miqdarda sözü html faylına at.
 for (let i = 0; i < 30; i++) {
     getWord();
 }
 
 function getWord() {
-    let word = document.createElement('div')
-    word.className = 'word'
+    let word = document.createElement('div');
+    word.className = 'word';
 
-    //listdən sözü götürüb hərflərin elementlər olduğu array-ə çevir
-    let tempWord = list[Math.floor(Math.random() * 1400)]
+    let tempWord = list[Math.floor(Math.random() * 1400)];
     tempWord = tempWord.split('');
     tempWord.push(' ');
 
-    //array-i  word div-inə əlavə elə faylına əlavə elə.
     for (let i = 0; i < tempWord.length; i++) {
         let letters = document.createElement('span');
         letters.className = 'active';
@@ -22,55 +19,72 @@ function getWord() {
         word.appendChild(letters);
     }
 
-    //word div-ini html faylına əlavə elə, hərflərin element olduğu array-i return elə.
-    document.querySelector('.test').appendChild(word)
+    document.querySelector('.test').appendChild(word);
 }
 
-let test = document.querySelectorAll('.active'); //testdə üzərində əməliyyat aparılmamış bütün hərflər və boşluqlar
-let i = 0; //kursorun mətn indeksi
-let j = 0; //kursorun söz indeksi
-let words = document.querySelectorAll('.word')
-let sum = words[0].textContent.length; //test-də istifadə olunan hər sözün uzunluğu
+let testActive = document.querySelectorAll('.active'); 
+let i = 0; 
+let j = 0; 
+let words = document.querySelectorAll('.word');
+let sum = words[0].textContent.length; 
+let correctWords = 0;
 
 function handleKey(e) {
-    if (e.key === test[i].textContent) {
 
-        //əgər doğru hərf basılıbsa hərfi yaşıl elə.
-        test[i].className = 'passed';
+    if (e.key === testActive[i].textContent) {
+
+        testActive[i].className = 'passed';
         i++;
 
     }  else if (e.key !== ' '){
-        //əgər yanlış hərf-ə basılıb və boşluğa basılmıyıbsa hərfi qırmızı elə
-        test[i].className = 'incorrect';
+        testActive[i].className = 'incorrect';
 
     }
 
     
     if  (e.key === ' ') {
-       
+    
+        let passedLetters = words[j].querySelectorAll('.passed'); 
 
-        let passedLetters = words[j].querySelectorAll('.passed') //kursorun içində olduğu sözdəki doğru basılmış hərflər
-
-        // əgər sözdəki bütün hərflər doğru basılmayıb və növbəti sözə keçilibsə (space basılıbsa) bütün  
-        // hərfləri qırmızı elə
         if (passedLetters.length !== words[j].textContent.length) { 
-            words[j].classList.add('error')
+            words[j].classList.add('error');
+        } else {
+            words[j].classList.add('correct');
+            correctWords++;
         }
 
-        //əgər növbəti söz varsa, kursoru növbəti sözün 0-cı indeksinə apar
-        i = sum;
-        sum += words[j + 1].textContent.length;
-        j++;
+
+
+        if(i+j >= 114) {
+            let passed = document.querySelectorAll('.correct, .error')
+            passed.forEach( object => {
+                object.remove();
+            })
+            
+            i=0;
+            j=0;
+            words = document.querySelectorAll('.word');
+            testActive = document.querySelectorAll('.active');
+            sum = words[0].textContent.length;
+            
+        } else {
+            i = sum;
+            sum += words[j + 1].textContent.length;
+            j++;
+        }        
+
+        
     }   
 }
+document.addEventListener('keypress', handleKey);
 
-let seconds=9;
+let seconds=59;
 let timer;
 
 function handleTimer() {
   if(!timer) {
     timer = window.setInterval(function() {
-        if(seconds < 10) {
+        if(seconds < 60) {
             document.getElementById("timer").innerHTML = seconds;
           }
              if (seconds >0 ) {
@@ -78,13 +92,12 @@ function handleTimer() {
              } else {
                 document.removeEventListener('keypress', handleKey);
                 clearInterval(timer);
-             };
+                console.log(correctWords)
+            };
     }, 1000);
   }
 } 
-
-document.addEventListener('keypress', handleKey)
-document.addEventListener('keypress', handleTimer)
+document.addEventListener('keypress', handleTimer);
 
 
 
