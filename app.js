b90b3ea4b75edd750ca4b923c7daf7f7f79640a4
DannyMay9082 <Danny.May.9012@gmail.com>
2022-07-08 16:59:42 +0400

    fix caret bugs

--- app.js
+++ app.js
@@ -28,38 +28,51 @@ let j = 0;
 let words = document.querySelectorAll('.word');
 let sum = words[0].textContent.length; 
 let correctWords = 0;
-testActive[i].className = 'caret';
+testActive[i].classList.add('caret')
 
 function handleKey(e) {
 
-    testActive[i+1].className = 'caret';
-
     if (e.key === testActive[i].textContent) {
 
+        testActive[i+1].classList.add('caret');
         testActive[i].className = 'passed';
         i++;
-
+        
     }  else if (e.key !== ' '){
+
         testActive[i].className = 'error';
+        testActive[i+1].classList.add('caret');
 
     }
 
-    
+
+
     if  (e.key === ' ') {
-    
+        
+        testActive[i].classList.remove('caret')        
+        testActive[i+1].classList.remove('caret')
+
         let passedLetters = words[j].querySelectorAll('.passed'); 
 
         if (passedLetters.length !== words[j].textContent.length) { 
+
             words[j].classList.add('error');
+
         } else {
+
             words[j].classList.add('correct');
             correctWords++;
+
         }
 
-        if(i >= 150) {
+        if(i >= 125) {
+
             let passed = document.querySelectorAll('.correct, .error')
+           
             passed.forEach( object => {
+
                 object.remove();
+            
             })
             
             i=0;
@@ -69,15 +82,19 @@ function handleKey(e) {
             sum = words[0].textContent.length;
             
         } else {
+
             i = sum;
             sum += words[j + 1].textContent.length;
             j++;
+        
         }        
 
+        testActive[i].classList.add('caret');
         
     }   
 }
-document.addEventListener('keypress', handleKey);
+
+document.addEventListener('keydown', handleKey);
 
 let seconds=29;
 let timer;
@@ -90,6 +107,8 @@ function handleTimer() {
                  seconds--;
              } else {
                 clearInterval(timer);
+                document.removeEventListener('keypress',handleKey);
+
                 alert(`Your Typing speed is: ${correctWords*2}WPM`)
             };
     }, 1000);
